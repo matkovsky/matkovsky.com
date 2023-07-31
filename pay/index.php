6a3e050dc714e19f347ffbc1e588863e10ca3748
Peter Matkovsky <peter@kuix.co>
2023-07-31 20:28:10 +0200

    Add Pay page

---
+++ pay/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<?php include_once(__DIR__ . '/../snippets/head.php') ?>
+	<link rel="stylesheet" href="/css/pay.css?v=2023_07_31" media="screen">
+	<title>Pay Peter</title>
+
+</head>
+<body>
+
+	<div class="pay-options">
+
+		<header>
+			<img src="/img/portrait.png" alt="Portrait of Peter">
+			<h1>Send me money</h1>
+		</header>
+
+		<article>
+			<section>
+				<h2>Revolut</h2>
+				<p>
+					<a href="https://revolut.me/matkovsky" target="_blank" rel="nofollow noopener noreferrer">
+						revolut.me/matkovsky
+					</a>
+				</p>
+			</section>
+			<section>
+				<h2>PayPal</h2>
+				<p>
+					<a href="https://paypal.me/pmatkovsky" target="_blank" rel="nofollow noopener noreferrer">
+						paypal.me/pmatkovsky
+					</a>
+				</p>
+			</section>
+			<section>
+				<h2>Tatra banka Slovensko</h2>
+				<p>
+					EUR<br>
+					2928886565/1100<br>
+					IBAN: SK18 1100 0000 0029 2888 6565<br>
+					BIC/SWIFT: TATRSKBX
+				</p>
+			</section>
+			<section>
+				<h2>CIB Magyarország</h2>
+				<p>
+					HUF<br>
+					10701269-65094879-51100005<br>
+					IBAN: HU71 1070 1269 6509 4879 5110 0005<br>
+					BIC/SWIFT: CIBHHUHB
+				</p>
+			</section>
+		</article>
+
+	</div>
+
+	<?php include_once(__DIR__ . '/../snippets/foot.php') ?>
+
+</body>
+</html>
--- snippets/head.php
+++ snippets/head.php
@@ -4,7 +4,7 @@
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;600&display=swap" rel="stylesheet">
 
 <link rel="stylesheet" href="/css/reset.css?v=2023_07_31" media="screen">
 <link rel="stylesheet" href="/css/main.css?v=2023_07_31" media="screen">
